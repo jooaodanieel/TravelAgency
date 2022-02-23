4966f56377bbc4009044ef956c5caba99fc5f087
João Daniel <jotaf.daniel@gmail.com>
2022-02-23 16:19:26 -0300

    add ktorm dependency
    it was lost during swapping from maven to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val ktorm_version: String by project
+val pgdriver_version: String by project
 
 plugins {
     application
@@ -27,6 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.ktorm:ktorm-core:${ktorm_version}")
+    implementation("org.postgresql:postgresql:${pgdriver_version}")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
