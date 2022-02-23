41b484e25003733c9f180736b43f8e33bf1a451c
João Daniel <jotaf.daniel@gmail.com>
2022-02-22 22:57:36 -0300

    add customer subdomain CreateCustomer service

---
+++ src/main/kotlin/com/techandteach/customer/application/services/CreateCustomer.kt
@@ -0,0 +1,24 @@
+package com.techandteach.customer.application.services
+
+import com.techandteach.customer.model.*
+import com.techandteach.customer.model.types.Email
+import com.techandteach.customer.model.types.Name
+import com.techandteach.customer.model.types.Password
+
+class CreateCustomer(private val customerRepository: CustomerRepository) {
+    private fun validate(name: Name, email: Email) {
+        if (customerRepository.isNameTaken(name.toString())) throw IllegalArgumentException("Customer name \"$name\" already in use")
+        if (customerRepository.isEmailTaken(email.toString())) throw IllegalArgumentException("Customer email \"$email\" already in use")
+    }
+
+    fun create(maybeName: String, maybeEmail: String, maybePassword: String): Customer {
+        val name = Name.fromString(maybeName)
+        val email = Email.fromString(maybeEmail)
+        val password = Password.fromString(maybePassword)
+
+        validate(name, email)
+
+        val customer = Customer.create(name, email, password)
+        return customerRepository.add(customer)
+    }
+}
